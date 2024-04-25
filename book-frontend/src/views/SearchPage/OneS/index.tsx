cee151e70a96ea13f5e398efd7e15b001fb7ad50
dotting <dotting103@gmail.com>
2024-04-26 01:53:06 +0900

    git ignore update tsconfig update

--- book-frontend/src/views/SearchPage/OneS/index.tsx
+++ book-frontend/src/views/SearchPage/OneS/index.tsx
@@ -1,48 +1,35 @@
 import { useEffect, useState } from 'react';
 import jsonData from '../../../assets/projdb_comp.json'; // Import the JSON file
 
-const 
+
 
 function searchedList() {
-  const 
+  const keyword = '김밥';
 
-  const keyword = "김밥";
 
-    if (keyword) {
-      const restaurant = jsonData.rows.find((row) => row[3] === keyword);
-      if (restaurant) {
-        setRestaurantData(restaurant);
-      } else {
-        console.error('Restaurant not found in data.');
-      }
-    }
-  }, [restaurantName]);
+  const lists = jsonData.rows.filter((restaurant) => {if(restaurant[3] != null) restaurant[3].includes(keyword)});
+  
 
-  const handleInputChange = (event) => {
-    setRestaurantName(event.target.value);
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter restaurant name"
-        value={restaurantName}
-        onChange={handleInputChange}
-      />
-      {restaurantData ? (
-        <div>
-          <h2>Restaurant Information:</h2>
-          <p>Name: {restaurantData[3]}</p>
-          <p>Phone: {restaurantData[0]}</p>
-          <p>Address: {restaurantData[2]}</p>
-          {/* Add more fields as needed */}
-        </div>
-      ) : (
-        <p>No data available for the specified restaurant.</p>
-      )}
-    </div>
-  );
+  return(
+    <>
+      {lists.map((item) => (
+        <Card key={item} {...item} />  // 잔여연산자 사용
+      ))}
+    </>)
 }
+  
+  // Card component
+  
+  
+  function Card(key) {   
+    return (
+      <div className='cardContainer'>
+        <h2>상호명   {key[3]}</h2>
+        <h2>품목 {key[4]}</h2>
+        <h2>전화번호 {key[0]}</h2>
+        <h2>네이버 url {key[7]}</h2>
+      </div>
+    );
+  }
 
 export default searchedList;
